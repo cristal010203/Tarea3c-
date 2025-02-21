8866a08abbc535f0c40b7917914fe272490dbdbc
Cmunoz <cmunozm6@miumg.edu.gt>
2025-02-21 12:19:51 -0600

    se agrega el primer ejercicio de la iterativa for

---
+++ iterativa1for.cpp
@@ -0,0 +1,12 @@
+//Desarrolla un programa que imprima los números pares
+// del 2 al 20.
+#include<iostream>
+using namespace std;
+
+int main(){
+  for(int i=2;i<20;i+=2){
+    cout<<i<<" "<<endl;
+  }
+  cout<<endl;
+  return 0;
+}
