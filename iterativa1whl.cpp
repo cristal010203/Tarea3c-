b1e1b0e8f0b73cdb35fa6004c5116d82feeac0cc
Cmunoz <cmunozm6@miumg.edu.gt>
2025-02-20 23:21:09 -0600

    Se agrega el primer ejercicio de la iterativa while

---
+++ iterativa1whl.cpp
@@ -0,0 +1,14 @@
+//Crea un programa que imprima los números del 1 al 20 en
+// orden ascendente.
+#include<iostream>
+using namespace std;
+
+int main(){
+  int num = 1;
+  while(num <= 20){
+    cout << num <<" "<<endl;
+    num++;
+  }
+  cout << endl;
+  return 0;
+ }
